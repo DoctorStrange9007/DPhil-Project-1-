08ec9a2aa389b08625f85efbc72b87c8b95b87bc
Jesse Hoekstra <jesse.hoekstra@ziggo.nl>
2024-09-20 15:58:18 +0200

    Added model class and first model setup.

---
+++ src/model.py
@@ -0,0 +1,19 @@
+class Model:
+    """Base class of models"""
+
+    def __init__(self, data_obj, name):
+        self._data_obj = data_obj 
+        self._name = name 
+
+class Model1(Model):
+    """Model 1"""
+
+    def __init(self, data_obj, name, truncate=False):
+        super().__init__(data_obj, name)
+        self._truncate = truncate
+
+    def calculate(self, confidence):
+        time_series = self._data_obj.yearly_ts()
+         
+        pass 
+    
