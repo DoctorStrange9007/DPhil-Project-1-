3c51d2f2cfd552ac3a670d62bc9fafcf0875840b
Jesse Hoekstra <jesse.hoekstra@ziggo.nl>
2024-10-31 22:33:49 +0000

    first data acquiring steps.

--- src/read_data.py
+++ src/read_data.py
@@ -17,11 +17,22 @@ class ReadData:
     def __init__(self, run_sett: dict):
         """constructs a ReadData object"""
         self._run_sett = run_sett
-        input_path = os.path.join(run_sett["input_dir"], run_sett["input_fn"])
-        raw_data = pd.read_csv(input_path + ".csv", parse_dates=["Period"])
+
         # self.data = self.parse_data(data=raw_data)
-        self.data = raw_data  # change, but now impoty Mihai's data directly
+        self.data = self.read_raw()
         self.data_library = {}
+
+    def read_raw(self):
+        sorted_strings = sorted(
+            list_visible_files_with_list_comprehension(self._run_sett["input_dir"]),
+            key=lambda x: int(x[:8]),
+        )
+        for day_file in sorted_strings:
+            input_path = os.path.join(self._run_sett["input_dir"], day_file)
+            raw_data = pd.read_csv(
+                input_path, index_col="ticker", compression="gzip"
+            ).loc[:, "close"]
+            # now keep merging days to that series in dfs
 
     def parse_data(self, data: pd.DataFrame) -> pd.DataFrame:
         """Applies some global cleaning of the data.
@@ -37,3 +48,8 @@ class ReadData:
         Returns:
             data: parsed input data
         """
+
+
+def list_visible_files_with_list_comprehension(directory):
+    visible_files = [file for file in os.listdir(directory) if not file.startswith(".")]
+    return visible_files
